4c7a732ffbf323b859fc6b10c79652492be4c7f4
khurram-syed <khurram245@yahoo.com>
2026-01-11 13:26:09 +0000

    DriverManager updated for headless run

--- src/test/java/driver/DriverManager.java
+++ src/test/java/driver/DriverManager.java
@@ -4,8 +4,13 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class DriverManager {
     private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
@@ -14,27 +19,42 @@ public class DriverManager {
     public static WebDriver getDriver() {
         if (DRIVER.get() == null) {
           String browser = System.getProperty("browser","chrome").toLowerCase();
+          boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+
           WebDriver driver;
           switch(browser){
               case "firefox":
                   WebDriverManager.firefoxdriver().setup();
-                  driver = new FirefoxDriver();
+                  FirefoxOptions firefoxOptions = new FirefoxOptions();
+                  if (headless) firefoxOptions.addArguments("--headless");
+                  driver = new FirefoxDriver(firefoxOptions);
                   break;
               case "edge":
                   WebDriverManager.edgedriver().setup();
-                  driver = new EdgeDriver();
+                  EdgeOptions edgeOptions = new EdgeOptions();
+                  if (headless) edgeOptions.addArguments("--headless");
+                  driver = new EdgeDriver(edgeOptions);
                   break;
               case "safari":
                   WebDriverManager.safaridriver().setup();
-                  driver = new FirefoxDriver();
+                  driver = new SafariDriver();
                   break;
               case "chrome":
               default:
                   WebDriverManager.chromedriver().setup();
-                  driver = new ChromeDriver();
+                  ChromeOptions chromeOptions = new ChromeOptions();
+                  if (headless) {
+                      chromeOptions.addArguments(
+                              "--headless=new",
+                              "--disable-gpu",
+                              "--window-size=1920,1080",
+                              "--no-sandbox",
+                              "--disable-dev-shm-usage"
+                      );
+                  }
+                  driver = new ChromeDriver(chromeOptions);
                   break;
           }
-            driver.manage().window().maximize();
             DRIVER.set(driver);
         }
         return DRIVER.get();
